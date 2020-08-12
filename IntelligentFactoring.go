5e437ae298a77724cd5128e996518821a6f2e072
logan aikas <laikas@gmail.com>
2020-08-12 10:43:05 -0700

    succesful foil

--- IntelligentFactoring.go
+++ IntelligentFactoring.go
@@ -3,6 +3,7 @@ package main
 import (
 
 	"fmt"
+	"sort"
 	"strconv"
 )
 
@@ -24,10 +25,13 @@ import (
 func main() {
 	
 
-	equation := [][]float64{gOP(), gOP(), gNumsAdded(2, 3, 3, 0), gCP(), gMultiply(), gOP(), gOP(), gNumsAdded(3, 2, 1, 1, 3, 0), gCP(), gMultiply(), gOP(), gNumsAdded(2, 1, 1, 0), gCP(), gCP(), gCP()}       
+	equation := [][]float64{gOP(), gOP(), gNum(2, 3, 3, 0), gCP(2),  gOP(), gOP(), gNum(3, 2, 1, 1, 3, 0), gCP(1), gOP(), gNum(2, 1, 1, 0), gCP(1), gCP(1), gCP(1)}       
 
 	fmt.Println(DecodeFloatSliceToEquation(equation))
 
+	foiledEquation := FoilAllNeighboringParenthesis(equation)
+
+	fmt.Println(DecodeFloatSliceToEquation(foiledEquation))
 }
 
 
@@ -56,33 +60,28 @@ func DecodeFloatSliceToEquation(equation [][]float64 ) string {
 			if(IsOP(firstIndex, secondIndex)){
 				equationString += "( "
 				depthLevel++ 
-			}else if(IsCP(firstIndex, secondIndex)){
+			}else if(IsCP(currentItem)){
 				equationString += " )"
+				if(currentItem[j+2] != 0 ){
+					equationString += "^" + strconv.FormatFloat(currentItem[2], 'f', -1, 64) + " "
+				}
 				depthLevel--
-			}else if(IsPlus(firstIndex, secondIndex)){
-				equationString += " + "
-			}else if(IsMinus(firstIndex, secondIndex)){
-				equationString += " - "
-			}else if(IsMultiply(firstIndex, secondIndex)){
-				equationString += " * "
-			}else if(IsDivide(firstIndex, secondIndex)){
-				equationString += " / "
 			}else if(IsNumber(firstIndex)){
 				if(firstIndex != 1){
 					if(secondIndex == 0){
-						equationString += firstIndexString + " "
+						equationString += firstIndexString + " + "
 					}else if(secondIndex == 1){
-						equationString += firstIndexString + "S "
+						equationString += firstIndexString + "S + "
 					}else{
-						equationString += firstIndexString + "S^" + secondIndexString + " "
+						equationString += firstIndexString + "S^" + secondIndexString + " + "
 					}
 				}else{
 					if(secondIndex == 0){
-						equationString += firstIndexString + " "
+						equationString += firstIndexString + " + "
 					}else if(secondIndex == 1){
-						equationString += "S "
+						equationString += "S + "
 					}else{
-						equationString += "S^" + secondIndexString + " "
+						equationString += "S^" + secondIndexString + " +"
 					}
 				}
 				
@@ -91,6 +90,10 @@ func DecodeFloatSliceToEquation(equation [][]float64 ) string {
 			}
 			fmt.Println(equationString)
 			fmt.Println(depthLevel)
+
+			if(IsCP(currentItem)){
+				break	
+			}
 
 		}
 
@@ -124,127 +127,12 @@ func gNum(nums ...float64) []float64 {
 
 }
 
-func gNumsAdded(nums ...float64) []float64 {
-
-	if( (len(nums)%2) != 0){
-		panic("error, invalid amount of numbers")
-	}
-
-	returnSlice := []float64{}
-
-	for i := 0; i < len(nums); i = (i + 2) {
-
-		
-
-		returnSlice = append(returnSlice, nums[i])
-		returnSlice = append(returnSlice, nums[i+1])
-		if(!((i+2) >= len(nums))){
-			returnSlice = append(returnSlice, gPlus()...)
-		}
-
-	}
-
-	return returnSlice
-
-}
-
-
-func gNumsSubtracted(nums ...float64) []float64 {
-
-	if( (len(nums)%2) != 0){
-		panic("error, invalid amount of numbers")
-	}
-
-	returnSlice := []float64{}
-
-	for i := 0; i < len(nums); i = (i + 2) {
-
-		
-
-		returnSlice = append(returnSlice, nums[i])
-		returnSlice = append(returnSlice, nums[i+1])
-
-		if(!((i+2) >= len(nums))){
-			returnSlice = append(returnSlice, gMinus()...)
-		}
-
-	}
-
-	return returnSlice
-
-}
-
-
-func gNumsMultiplied(nums ...float64) []float64 {
-
-	if( (len(nums)%2) != 0){
-		panic("error, invalid amount of numbers")
-	}
-
-	returnSlice := []float64{}
-
-	for i := 0; i < len(nums); i = (i + 2) {
-
-		
-
-		returnSlice = append(returnSlice, nums[i])
-		returnSlice = append(returnSlice, nums[i+1])
-
-		if(!((i+2) >= len(nums))){
-			returnSlice = append(returnSlice, gMultiply()...)
-		}
-
-	}
-
-	return returnSlice
-
-}
-func gNumsDivided(nums ...float64) []float64 {
-
-	if( (len(nums)%2) != 0){
-		panic("error, invalid amount of numbers")
-	}
-
-	returnSlice := []float64{}
-
-	for i := 0; i < len(nums); i = (i + 2) {
-
-		
-
-		returnSlice = append(returnSlice, nums[i])
-		returnSlice = append(returnSlice, nums[i+1])
-
-		if(!((i+2) >= len(nums))){
-			returnSlice = append(returnSlice, gDivide()...)
-		}
-
-	}
-
-	return returnSlice
-
-}
 func gOP() []float64 {
 	return []float64{0, 0}
 }
 
-func gCP() []float64 {
-	return []float64{0, 1}
-}
-
-func gPlus() []float64 {
-	return []float64{0, 2}
-}
-
-func gMinus() []float64 {
-	return []float64{0, 3}
-}
-
-func gMultiply() []float64 {
-	return []float64{0, 4}
-}
-
-func gDivide() []float64 {
-	return []float64{0, 5}
+func gCP(exponent float64) []float64 {
+	return []float64{0, 1, exponent}
 }
 
 
@@ -266,11 +154,11 @@ func FoilAllNeighboringParenthesis(equation [][]float64) [][]float64 {
 
 	numbersToFoilSecondTerm := []float64{}
 
-	previousTerm := []float64{}
+	previousTerm := []float64{0, -1}
 
-	outerAndInnerMarkerForFoilStart := []int{}
+	foilStart := -1
 
-	outerAndInnerMarkerForFoilEnd := []int{}
+	foilEnd := -1
 
 	foundFoil := false
 
@@ -291,201 +179,116 @@ func FoilAllNeighboringParenthesis(equation [][]float64) [][]float64 {
 			previousFirstIndex := previousTerm[0]
 			previousSecondIndex := previousTerm[1]
 
+			fmt.Println("current item", currentItem)
+			fmt.Println("previous item", previousTerm)
+
 			if(IsOP(firstIndex, secondIndex)){
+				
+
+				fmt.Println("is OP")
 				depthLevel++ 
 				if(foilStateMachine == 0){
 					foilStateMachine = 1
-					outerAndInnerMarkerForFoilStart = []int{i, j}					
+					foilStart = i					
 				}
 				
 				
 				if(IsOP(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsCP(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsPlus(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsMinus(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsMultiply(previousFirstIndex, previousSecondIndex)){
-					if(foilStateMachine == 5){
-						foilStateMachine = 6
+					fmt.Println("previous is OP")
+					if(foilStateMachine == 4){
+						foilStateMachine = 1
+						numbersToFoilFirstTerm = []float64{}
+						numbersToFoilSecondTerm = []float64{}
+						foilStart = i
 					}
-				}else if(IsDivide(previousFirstIndex, previousSecondIndex)){
+				}else if(IsCP(previousTerm)){
+					fmt.Println("previous is CP")
+					if(foilStateMachine == 3){
+						foilStateMachine = 4
+					}
 					
 				}else if(IsNumber(previousFirstIndex)){
+					fmt.Println("previous is Numbers")
+				}else{
+					
 					//-1 is given for the previous term second index of the first
 					//cycle since there is no previous term 
 					//this is the only place in all if else needed to check
 					//since first term is always opening parenthesis
-					if(firstIndex == 0 && secondIndex != -1){
-
+					if(previousFirstIndex == 0 && previousSecondIndex != -1){
+						panic("unknown equation item FoilAllNeighboringParenthesis()")	
 					}	
-				}else{
-					panic("unknown equation item FoilAllNeighboringParenthesis()")	
+					
 				}
-			}else if(IsCP(firstIndex, secondIndex)){
+			}else if(IsCP(currentItem)){
+
+				fmt.Println("is CP")
+
 				depthLevel--
 				if(IsOP(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsCP(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsPlus(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsMinus(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsMultiply(previousFirstIndex, previousSecondIndex)){
-
-				}else if(IsDivide(previousFirstIndex, previousSecondIndex)){
-					
+					fmt.Println("previous is OP")
+				}else if(IsCP(previousTerm)){
+					fmt.Println("previous is CP")
 				}else if(IsNumber(previousFirstIndex)){
-					if(foilStateMachine == 2){
-						foilStateMachine = 4
-					}
-					if(foilStateMachine == 7){
-						foilStateMachine = 9
-						outerAndInnerMarkerForFoilEnd = []int{i, j}
-					}
-				}else{
-					panic("unknown equation item FoilAllNeighboringParenthesis()")	
-				}						
-			}else if(IsPlus(firstIndex, secondIndex)){
-				if(IsOP(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsCP(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsPlus(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsMinus(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsMultiply(previousFirstIndex, previousSecondIndex)){
-
-				}else if(IsDivide(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsNumber(previousFirstIndex)){
-
-				}else{
-					panic("unknown equation item FoilAllNeighboringParenthesis()")	
-				}						
-			
-			}else if(IsMinus(firstIndex, secondIndex)){
-				if(IsOP(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsCP(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsPlus(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsMinus(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsMultiply(previousFirstIndex, previousSecondIndex)){
-
-				}else if(IsDivide(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsNumber(previousFirstIndex)){
-
-				}else{
-					panic("unknown equation item FoilAllNeighboringParenthesis()")	
-				}						
-			
-			}else if(IsMultiply(firstIndex, secondIndex)){
-				if(IsOP(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsCP(previousFirstIndex, previousSecondIndex)){
-					if(foilStateMachine == 4){
-						foilStateMachine = 5
-					}
-				}else if(IsPlus(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsMinus(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsMultiply(previousFirstIndex, previousSecondIndex)){
-
-				}else if(IsDivide(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsNumber(previousFirstIndex)){
-
-				}else{
-					panic("unknown equation item FoilAllNeighboringParenthesis()")	
-				}						
-			
-			}else if(IsDivide(firstIndex, secondIndex)){
-				if(IsOP(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsCP(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsPlus(previousFirstIndex, previousSecondIndex)){
+					fmt.Println("previous is Numbers")
 					if(foilStateMachine == 2){
 						foilStateMachine = 3
 					}
-					if(foilStateMachine == 7){
-						foilStateMachine = 8
+					if(foilStateMachine == 5){
+						foilStateMachine = 6
+						foilEnd = i
+						foundFoil = true
 					}
-				}else if(IsMinus(previousFirstIndex, previousSecondIndex)){
-					if(foilStateMachine == 3){
-						foilStateMachine = 2
-					}
-					if(foilStateMachine == 7){
-						foilStateMachine = 8
-					}
-				}else if(IsMultiply(previousFirstIndex, previousSecondIndex)){
-
-				}else if(IsDivide(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsNumber(previousFirstIndex)){
-
 				}else{
+					fmt.Println(currentItem)
 					panic("unknown equation item FoilAllNeighboringParenthesis()")	
 				}						
-			
-			}else if(IsNumber(firstIndex, secondIndex)){
+			}else if(IsNumber(firstIndex)){
+
+				fmt.Println("is Numbers")
+
 
 				if(IsOP(previousFirstIndex, previousSecondIndex)){
+					fmt.Println("previous is OP")
 					if(foilStateMachine == 1){
 						foilStateMachine = 2
-						numbersToFoilFirstTerm = append(numbersToFoilFirstTerm, g(firstIndex, secondIndex))
+						numbersToFoilFirstTerm = append(numbersToFoilFirstTerm, gNum(firstIndex, secondIndex)...)
 					}
-
-					if(foilStateMachine == 6){
-						foilStateMachine = 7
-						numbersToFoilFirstTerm = append(numbersToFoilFirstTerm, g(firstIndex, secondIndex))
+					if(foilStateMachine == 4){
+						foilStateMachine = 5
+						numbersToFoilSecondTerm = append(numbersToFoilSecondTerm, gNum(firstIndex, secondIndex)...)
 					}
-				}else if(IsCP(previousFirstIndex, previousSecondIndex)){
-					
-				}else if(IsPlus(previousFirstIndex, previousSecondIndex)){
-					if(foilStateMachine == 3){
-						numbersToFoilFirstTerm = append(numbersToFoilFirstTerm, g(firstIndex, secondIndex))
-						foilStateMachine = 2
-					}
-					if(foilStateMachine == 8){
-						foilStateMachine = 7
-						numbersToFoilFirstTerm = append(numbersToFoilFirstTerm, g(firstIndex, secondIndex))
-					}
-				}else if(IsMinus(previousFirstIndex, previousSecondIndex)){
-					if(foilStateMachine == 3){
-						numbersToFoilFirstTerm = append(numbersToFoilFirstTerm, g(firstIndex * - 1, secondIndex * -1))
-						foilStateMachine = 2
-					}
-					if(foilStateMachine == 8){
-						foilStateMachine = 7
-						numbersToFoilFirstTerm = append(numbersToFoilFirstTerm, g(firstIndex * -1 , secondIndex * -1))
-					}
-				}else if(IsMultiply(previousFirstIndex, previousSecondIndex)){
-
-				}else if(IsDivide(previousFirstIndex, previousSecondIndex)){
-					
+				}else if(IsCP(previousTerm)){
+					fmt.Println("previous is CP")
 				}else if(IsNumber(previousFirstIndex)){
-
+					fmt.Println("previous is Numbers")
+					if(foilStateMachine == 2){
+						foilStateMachine = 2
+						numbersToFoilFirstTerm = append(numbersToFoilFirstTerm, gNum(firstIndex, secondIndex)...)
+					}
+					if(foilStateMachine == 5){
+						foilStateMachine = 5
+						numbersToFoilSecondTerm = append(numbersToFoilSecondTerm, gNum(firstIndex, secondIndex)...)
+					}
 				}else{
+					fmt.Println(currentItem)
 					panic("unknown equation item FoilAllNeighboringParenthesis()")	
 				}												
 			}else{
+				fmt.Println(currentItem)
 				panic("unknown equation item FoilAllNeighboringParenthesis()")	
 			}						
 
-			if(foilStateMachinePrevious == foilStateMachine){
+
+			fmt.Println("previous foil state machine", foilStateMachinePrevious)
+			fmt.Println("current foil state machine", foilStateMachine)
+
+			if( (foilStateMachinePrevious == foilStateMachine) && foilStateMachine != 1 && foilStateMachine != 2 && foilStateMachine != 5 && foilStateMachine != 0 && foilStateMachine != 4){
 				foilStateMachine = 0
 				foilStateMachinePrevious = 0
-			}else if(foilStateMachine == 9){
+				numbersToFoilFirstTerm = []float64{}
+				numbersToFoilSecondTerm = []float64{}
+			}else if(foilStateMachine == 7){
 				foundFoil = true
 				break
 
@@ -493,30 +296,42 @@ func FoilAllNeighboringParenthesis(equation [][]float64) [][]float64 {
 				foilStateMachinePrevious = foilStateMachine
 			}
 
-			previousTerm = g(firstIndex, secondIndex)
+			previousTerm = currentItem
+
+			if(IsCP(currentItem)){
+				break
+			}
 
 		}
+
+		
+		
+
 
 	}
 
 
 	if(foundFoil){
 
+		fmt.Println(foilStart, foilEnd)
+
 		newSlice := []float64{}
 
-		for i := 0; i < len(numbersToFoilFirstTerm); i++ {
+		for i := 0; i < len(numbersToFoilFirstTerm); i = (i + 2) {
 
-			firstNum := numbersToFoilFirstTerm[i]
+			firstNumMultiplier := numbersToFoilFirstTerm[i]
 
-			for j := 0; j < len(numbersToFoilSecondTerm); j++ {
+			firstNumExponent := numbersToFoilFirstTerm[i+1]
 
-				secondNum := numbersToFoilSecondTerm[j]
+			for j := 0; j < len(numbersToFoilSecondTerm); j = (j + 2) {
 
-				newNum := []float64{firstNum[0]*secondNum[0], firstNum[1]*secondNum[1]}
+				secondNumMultiplier := numbersToFoilSecondTerm[j]
 
-				
+				secondNumExponent := numbersToFoilSecondTerm[j+1]
 
-				newSlice = append(newSlice
+				newNum := []float64{firstNumMultiplier*secondNumMultiplier, firstNumExponent+secondNumExponent}
+
+				newSlice = append(newSlice, newNum...)
 
 
 
@@ -524,15 +339,82 @@ func FoilAllNeighboringParenthesis(equation [][]float64) [][]float64 {
 
 		}
 
+		fmt.Println("first terms and second terms")
+		fmt.Println(numbersToFoilFirstTerm)
+		fmt.Println(numbersToFoilSecondTerm)
+
+		fmt.Println(newSlice)
+		newSlice = SimplifyLikeTermsEquationSectionAndSortByDescendningExponent(newSlice)
+		
+
+		return Substitute1DSliceInto2DSliceStartAndEnd(foilStart, foilEnd, newSlice, equation)
 
 
+	}else{
+		return equation
 	}
 
 
 }
 
 
+func SimplifyLikeTermsEquationSectionAndSortByDescendningExponent(equationSection []float64) []float64 {
 
+	termsMap := make(map[float64][]float64)
+
+	for i := 0; i < len(equationSection); i = (i + 2) {
+		if(termsMap[equationSection[i+1]] == nil){
+			termsMap[equationSection[i+1]] = []float64{equationSection[i]}
+		}else{
+			termsMap[equationSection[i+1]] = append(termsMap[equationSection[i+1]], equationSection[i])
+		}
+	}
+
+	exponentsSlice := []float64{}
+
+	for exponents, _ := range termsMap {
+		exponentsSlice = append(exponentsSlice, exponents)
+	}
+
+	// sort.Float64s(exponentsSlice)
+
+	// sort.Reverse(sort.Float64Slice(exponentsSlice))
+
+	sort.Sort(sort.Reverse(sort.Float64Slice(exponentsSlice)))
+
+	returnSlice := []float64{}
+
+	for i := 0; i < len(exponentsSlice); i++ {
+
+		currentMultipliers := termsMap[exponentsSlice[i]]
+
+		mutlipliersAdded := float64(0)
+
+		for j := 0; j < len(currentMultipliers); j++ {
+			mutlipliersAdded += currentMultipliers[j]
+		}
+
+		returnSlice = append(returnSlice, mutlipliersAdded)
+		returnSlice = append(returnSlice, exponentsSlice[i])
+
+
+	}
+
+	return returnSlice
+
+
+}
+
+
+func Substitute1DSliceInto2DSliceStartAndEnd(start int, end int, new1DSlice []float64, equation [][]float64) [][]float64{
+
+	returnSlice := append(equation[0:start], new1DSlice)
+
+	returnSlice = append(returnSlice, equation[(end + 1): len(equation)]...)
+
+	return returnSlice
+
+}
 
 func IsOP(num1 float64, num2 float64) bool {
 	if(num1 == 0 && num2 == 0){
@@ -541,36 +423,8 @@ func IsOP(num1 float64, num2 float64) bool {
 		return false
 	}
 }
-func IsCP(num1 float64, num2 float64) bool {
-	if(num1 == 0 && num2 == 1){
-		return true
-	}else{
-		return false
-	}
-}
-func IsPlus(num1 float64, num2 float64) bool {
-	if(num1 == 0 && num2 == 2){
-		return true
-	}else{
-		return false
-	}
-}
-func IsMinus(num1 float64, num2 float64) bool {
-	if(num1 == 0 && num2 == 3){
-		return true
-	}else{
-		return false
-	}
-}
-func IsMultiply(num1 float64, num2 float64) bool {
-	if(num1 == 0 && num2 == 4){
-		return true
-	}else{
-		return false
-	}
-}
-func IsDivide(num1 float64, num2 float64) bool {
-	if(num1 == 0 && num2 == 5){
+func IsCP(nums []float64) bool {
+	if(len(nums) == 3){
 		return true
 	}else{
 		return false
@@ -598,24 +452,21 @@ func CheckEquationForSyntaxErrors(equation [][]float64) {
 			firstIndex := currentItem[j]
 			secondIndex := currentItem[j+1]
 
+
+
 			if(i == 0 && !IsOP(firstIndex, secondIndex)){
 				panic("Syntax Error first item must be ( CheckEquationForSyntaxErrors()")
 			}
 
 			if(IsOP(firstIndex, secondIndex)){
 				depthLevel++ 
-			}else if(IsCP(firstIndex, secondIndex)){
+			}else if(IsCP(currentItem)){
 				depthLevel--
-			}else if(IsPlus(firstIndex, secondIndex)){
-				
-			}else if(IsMinus(firstIndex, secondIndex)){
-				
-			}else if(IsMultiply(firstIndex, secondIndex)){
-				
-			}else if(IsDivide(firstIndex, secondIndex)){
-				
+			}else if(IsNumber(firstIndex)){
+
 			}else{
-				
+				fmt.Println(currentItem)
+				panic("Syntax Error unknown item type CheckEquationForSyntaxErrors()")
 			}
 
 			//this would occur if there's more closing parenthesis than opening
@@ -623,6 +474,9 @@ func CheckEquationForSyntaxErrors(equation [][]float64) {
 				panic("Syntax Error too many ) for the number of ( CheckEquationForSyntaxErrors()")
 			}
 
+			if(len(currentItem) == 3){
+				break	
+			}
 		}
 	}
 
